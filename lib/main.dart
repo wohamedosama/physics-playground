9cd51f92b1a2fe3470ef4f5b3c13136e1bc795b2
DESKTOP-IA7BFTA\hp <mohamedmedo9932@gmail.com>
2025-09-27 18:19:12 +0300

    initiate the application

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PhysicsPlayground());
+}
+
+class PhysicsPlayground extends StatelessWidget {
+  const PhysicsPlayground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
